d8781395f2d8679287903abdf87c3e00bf27e66e
leigeco <leigeco@github.com>
2018-07-25 21:48:01 +0900

    2주차 과제

---
+++ hw_week2.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get('/hihi/:num',function(req, res){
+    res.send('abc='+req.params.num);
+//localhost:8080/hihi/1234567890 입력하면 abc=1234567890나옴
+})
+
+app.get('/hihi/:abc?',function(req, res){
+    res.send('abc='+req.query.abc);
+//localhost:8080/hihi?abc=1234567890 입력하면 abc=1234567890나옴
+})
+
+app.get('/hihi',function(req, res){
+    res.send('intro');
+})
+
+app.get('/hello',function(req, res){
+    res.send('Hello World!');
+})
+
+
+app.listen(8080, function(){
+    console.log('Server is running');
+})
